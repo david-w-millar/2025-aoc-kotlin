f8083a8ae12e4358f8e5c2ee0fcdd93a27849672
David W Millar <david.w.millar@gmail.com>
2025-12-03 20:12:44 -0500

    Use foojay toolchain resolver, concise code docs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
-rootProject.name = "2025-aoc-kotlin"
+// settings.gradle.kts
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" 
+}
+
+rootProject.name = "2025-aoc-kotlin"
