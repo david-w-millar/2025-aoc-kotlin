6b8d4412c6f566b74b9335e4965d5d0b912e5d06
David W Millar <david.w.millar@gmail.com>
2025-12-08 20:39:36 -0500

    (refactor) use a more conventional amper jvm/app project structure

---
+++ amper-project/day01/src/Main.kt
@@ -0,0 +1,65 @@
+import Utils.println
+
+fun part1(input: List<String>): Int {
+    var dial = 50
+    var zeroCount = 0
+
+    input.forEach {
+        val dir = it.first()
+        val n = requireNotNull(it.drop(1).toIntOrNull())
+        when (dir) {
+            'R' -> dial += n
+            'L' -> dial -= n
+        }
+        // Dafuq - I thought % was mod - it's not
+        dial = dial.mod(100)
+        if (dial == 0) zeroCount++
+    }
+
+    return zeroCount
+}
+
+private fun String.direction() = first()
+private fun String.rotateN() = drop(1).toInt()
+
+private class CountingDial(startingValue: Int = 50) {
+    var zeroCount = 0
+    var currentValue = startingValue
+        set(newVal) {
+            field = newVal.mod(100)
+            if (field == 0) zeroCount++
+        }
+
+    fun turn(operation: String) {
+        val n = operation.rotateN()
+        when (operation.direction()) {
+            'R' -> repeat(n) { currentValue++ }
+            'L' -> repeat(n) { currentValue-- }
+        }
+    }
+}
+
+fun part2(input: List<String>): Int {
+    val dial = CountingDial(50)
+    input.forEach {
+        dial.turn(it)
+    }
+    return dial.zeroCount
+}
+
+fun main() {
+//    val testInput by lazy { Utils.readInput("Day01_test") }
+//    val input by lazy { Utils.readInput("Day01") }
+    val testInput by lazy { Utils.readResourceInput("Day01_test") }
+    val input by lazy { Utils.readResourceInput("Day01") }
+
+    // test if implementation meets criteria from the description, like:
+    check(part1(testInput) == 3)
+    "--- Day 01: Part 1 ---".println()
+    "Solution: ${part1(input)}".println()
+
+    // test if implementation meets criteria from the description, like:
+    check(part2(testInput) == 6)
+    "--- Day 01: Part 2 ---".println()
+    "Solution: ${part2(input)}".println()
+}
---
+++ amper-project/utils/src/Utils.kt
@@ -0,0 +1,30 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+object Utils {
+    /** Converts string to md5 hash. */
+    fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
+
+    /** The cleaner shorthand for printing output. */
+    fun Any?.println() = println(this)
+
+    /** Reads lines from the given input txt file. */
+    fun readInput(name: String) = Path("problem_inputs/$name.txt").readText().trim().lines()
+
+    /**
+     * Reads lines from a module resource file
+     * Usage: readResourceLines("/Day01_test"")
+     */
+    fun readResourceText(path: String): String {
+        val resourceUrl = this::class.java.getResource(path)
+        requireNotNull(resourceUrl) { "Resource not found at: $path" }
+        return resourceUrl.readText()
+    }
+
+    /** Reads lines from an input file from a module's resources */
+    fun readResourceInput(name: String) = readResourceText("/$name.txt").trim().lines()
+}
